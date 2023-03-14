b6123a45b3968f108fe75b73d9c50d9ce92f72af
unisanta <unisanta@M223-20>
2023-03-13 21:25:45 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Funcionario
+    {
+        public string V_CPF { get; set; }
+        public string V_Nome { get; set; } 
+        public double V_ValorDia { get; set; }
+        public double V_DiasTrabalhados { get; set; }
+        public double V_Salario { get; set; }
+        public double PctIR { get; set; }
+        public double SalarioLiquidoC { get; set; }
+
+        public Funcionario() 
+        { 
+            V_CPF = "";
+            V_Nome = "";
+        }
+        public void CalculaSalario()
+        {
+            V_Salario = V_ValorDia * V_DiasTrabalhados;
+        }
+        public void CalculaIR()
+        {
+
+            if (V_Salario <= 1903.98)
+            {
+                PctIR = 0;
+            }
+            if (V_Salario <= 2826.65)
+            {
+                PctIR = V_Salario * (7.5 / 100);
+            }
+            if (V_Salario <= 3751.05)
+            {
+                PctIR = V_Salario * (15 / 100);
+            }
+            if (V_Salario <= 4664.68|| V_Salario >=4664.69)
+            {
+                PctIR = V_Salario * (22.5 / 100);
+            }
+        }
+            public void SalarioLiquido()
+            {
+                SalarioLiquidoC = V_Salario - PctIR;
+            }
+            
+        }   
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Calcular_Click_1(object sender, EventArgs e)
+        {
+            Funcionario funcionario = new Funcionario();
+
+
+            // atribui as propriedades
+            funcionario.V_CPF = txtCPF.Text;
+            funcionario.V_Nome = txtNome.Text;
+            funcionario.V_DiasTrabalhados = double.Parse(txtDias.Text);
+            funcionario.V_ValorDia = double.Parse(txtValorDia.Text);
+
+            funcionario.CalculaSalario();
+            txtSalario.Text = funcionario.V_Salario.ToString();
+
+            funcionario.CalculaIR();
+            txtValorIR.Text = funcionario.PctIR.ToString();
+
+            funcionario.SalarioLiquido();
+            txtSalarioLiq.Text = funcionario.SalarioLiquidoC.ToString();
+
+
+        }
+    }
+}
